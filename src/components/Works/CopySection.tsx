5f43a3a0faa080087f4a4f1c445f879c9823db14
NoggyPLZ <brett@bluetractor.com>
2025-06-30 15:53:44 -0500

    added animations to images and headers

--- src/components/Works/CopySection.tsx
+++ src/components/Works/CopySection.tsx
@@ -1,3 +1,9 @@
+import gsap from 'gsap'
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import { SplitText, ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(useGSAP, SplitText);
+
 type Copy = {
     title: string;
     client: string;
@@ -13,9 +19,28 @@ type CopyProps ={
 
 export default function CopySection(props: CopyProps){
     const {title, client, skills, body, even} = props.copy;
+    const titleRef = useRef<HTMLDivElement>(null);
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    useGSAP(() =>{
+        let split = SplitText.create(titleRef.current, {type: "words, chars"});
+        gsap.from(split.words,{
+            duration:.8,
+            stagger:0.05,
+            y:-100,
+            opacity:0,
+            scrollTrigger:{
+                trigger:titleRef.current,
+                toggleActions: "restart none none none"
+            },
+            ease: "expo.in"
+
+        })
+    }, {scope: containerRef})
+
     return ( 
-        <div className="flex flex-col basis-4/12 gap-3 md:order-2 order-1">
-            <h3 className="font-bigger text-9xl 2xl:text-[18rem]/[14.5rem] 2xl:pt-6 text-palered-500">{title}</h3>
+        <div ref={containerRef} className="flex flex-col basis-4/12 gap-3 md:order-2 order-1">
+            <h3 ref={titleRef} className="font-bigger text-9xl 2xl:text-[18rem]/[14.5rem] 2xl:pt-6 text-palered-500">{title}</h3>
             <h4 className={`text-2xl ${even ? `text-darkgray-500` : `text-palegray-500`}`}><span className="font-bold">Client:</span> {client}</h4>
             <h4 className={`text-2xl ${even ? `text-darkgray-500` : `text-palegray-500`}`}><span className="font-bold">Skills:</span> {skills}</h4>
             <p className={`text-2xl font-light ${even ? `text-darkgray-500` : `text-palegray-500`}`}>{body}</p>
--- src/components/Works/WorksImages.tsx
+++ src/components/Works/WorksImages.tsx
@@ -1,3 +1,10 @@
+import gsap from 'gsap'
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/all";
+import { ScrollTrigger } from 'gsap/all';
+gsap.registerPlugin(useGSAP, SplitText, ScrollTrigger);
+
 type Images = {
     image_1: string;
     image_2: string;
@@ -10,13 +17,40 @@ type WorkImageProps = {
 
 export default function WorksImages(props: WorkImageProps){
     const {image_1, image_2, image_3} = props.images;
+    const imageOneRef = useRef<HTMLImageElement>(null);
+    const imageTwoRef = useRef<HTMLImageElement>(null);
+    const imageThreeRef = useRef<HTMLImageElement>(null);
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    useGSAP(() =>{
+        gsap.set([imageOneRef.current, imageTwoRef.current, imageThreeRef.current], {opacity:1})
+        gsap.from(imageOneRef.current, {
+            scrollTrigger:{
+                trigger:imageOneRef.current,
+                toggleActions:"restart none none none"
+            },
+            opacity:0,
+            duration:1,
+            ease: "power1.in"
+        })
+        gsap.from([imageTwoRef.current, imageThreeRef.current],{
+            scrollTrigger:{
+                trigger:imageTwoRef.current,
+                toggleActions:"restart none none none"
+            },
+            opacity:0,
+            duration:1,
+            ease: "power1.in"
+        })
+    }, {scope: containerRef})
+
 
     return (
-        <div className="flex flex-col gap-8 basis-8/12 md:order-1 order-2">
-            <img src={image_1} className="rounded-4xl max-w-full" />
+        <div ref={containerRef} className="flex flex-col gap-8 basis-8/12 md:order-1 order-2">
+            <img ref={imageOneRef} src={image_1} className="rounded-4xl max-w-full" />
             <div className="flex flex-row gap-8">
-                <img src={image_2} className="w-1/2  rounded-4xl" />
-                <img src={image_3} className="w-1/2  rounded-4xl" />
+                <img ref={imageTwoRef} src={image_2} className="w-1/2  rounded-4xl" />
+                <img ref={imageThreeRef} src={image_3} className="w-1/2  rounded-4xl" />
             </div>
          </div>
     )
