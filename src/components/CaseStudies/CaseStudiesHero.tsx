4c24293c68df95cb2347431825a45147d617bdcd
Brett <brett@bluetractor.com>
2026-08-29 08:07:11 -0500

    fixing a height issue causing an animation bug

--- src/components/CaseStudies/CaseStudiesHero.tsx
+++ src/components/CaseStudies/CaseStudiesHero.tsx
@@ -33,7 +33,7 @@ export default function CaseStudiesHero({
   return (
     <div
       ref={containerRef}
-      className="px-8 pt-30 md:pt-20 bg-palered-500 flex flex-col md:flex-row"
+      className="px-8 pt-30 md:pt-20 bg-palered-500 flex flex-col md:flex-row min-h-[550px] md:min-h-[600px]"
     >
       <div className="flex flex-col justify-end md:pb-10">
         <h3
