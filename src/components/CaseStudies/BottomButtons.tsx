eb34a161ac46edfe962c82b2ad062ef88d0aef96
Brett <brett@bluetractor.com>
2026-09-02 08:18:51 -0500

    fixing button arrows on bottom of case studies

--- src/components/CaseStudies/BottomButtons.tsx
+++ src/components/CaseStudies/BottomButtons.tsx
@@ -15,10 +15,10 @@ export default function BottomButtons({ csIndex }: { csIndex: number }) {
     <div className="grid grid-cols-2 md:grid-cols-3 px-8 gap-2">
       <Link
         to={`/case/${previous.slug}`}
-        className="flex justify-center items-center p-12 font-bold hover:text-palegray-500 gap-2 bg-palegray-500 text-darkgray-500 hover:bg-palered-500 rounded-2xl order-2 md:order-1"
+        className="flex flex-col md:flex-row justify-center items-center p-12 font-bold hover:text-palegray-500 gap-2 bg-palegray-500 text-darkgray-500 hover:bg-palered-500 rounded-2xl order-2 md:order-1"
       >
-        <ArrowLeft strokeWidth={3} />
-        Previous Case Study
+        <ArrowLeft strokeWidth={3} className="order-1" />
+        <p className="order-2">Previous Case Study</p>
       </Link>
       <button
         className="flex justify-center items-center p-12 font-bold hover:text-palegray-500 gap-2 bg-palegray-500 text-darkgray-500 hover:bg-palered-500 rounded-2xl order-1 md:order-2 col-span-2 md:col-span-1 cursor-pointer"
@@ -32,9 +32,15 @@ export default function BottomButtons({ csIndex }: { csIndex: number }) {
       </button>
       <Link
         to={`/case/${next.slug}`}
-        className="flex justify-center items-center p-12 font-bold hover:text-palegray-500 gap-2 bg-palegray-500 text-darkgray-500 hover:bg-palered-500 rounded-2xl order-3"
+        className="flex flex-col md:flex-row justify-center items-center p-12 font-bold hover:text-palegray-500 gap-2 bg-palegray-500 text-darkgray-500 hover:bg-palered-500 rounded-2xl order-3"
       >
-        Next Case Study <ArrowRight strokeWidth={3} />
+        <p
+          className="
+        md:order-1 order-2"
+        >
+          Next Case Study
+        </p>
+        <ArrowRight strokeWidth={3} className="md:order-2 order-1" />
       </Link>
     </div>
   );
