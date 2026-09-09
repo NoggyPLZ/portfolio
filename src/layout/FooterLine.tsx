a49d6620b782e91b4aeca885ce2f6aa1407e0558
Brett <brett@bluetractor.com>
2026-09-09 12:54:51 -0500

    sped up the footerline animation

--- src/layout/FooterLine.tsx
+++ src/layout/FooterLine.tsx
@@ -17,7 +17,7 @@ export default function FooterLine() {
       });
       gsap.from(split.chars, {
         opacity: 0,
-        duration: 0.9,
+        duration: 0.7,
         ease: "steps.out",
         stagger: 0.05,
         scrollTrigger: {
